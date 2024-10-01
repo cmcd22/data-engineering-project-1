955ca4e7fc33468efa99de659a79bcb9f32a6b0a
Chris McDonald <87786853+cmcd22@users.noreply.github.com>
2024-10-01 17:32:01 +1300

    Create json_scraper.py

---
+++ json_scraper.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+def json_scraper(url, file_name, bucket):
+    print('Commence Running')
+    response = requests.request("GET", url)
+    json_data = response.json()
+
+    with open(file_name, 'w', encoding='utf-8') as json_file:
+        json.dump(json_data, json_file, ensure_ascii=False, indent=4)
+
+    print('Terminate Running')
+
+
+json_scraper('https://restcountries.com/v3.1/all',
+             'country_data.json', 'data-bucket-907')
